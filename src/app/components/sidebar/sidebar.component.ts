d4e3a5e22b8bdb9de4903cc7859624e440d3e7bc
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-03-20 15:35:52 -0300

    feat: adiciona interação do menu lateral, o reduzindo ou expandindo, caso seja realizado o clique no botão

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,14 +1,19 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgIf, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'sidebar-component',
   standalone: true,
-  imports: [NgOptimizedImage, NgbDropdownModule],
+  imports: [NgOptimizedImage, NgbDropdownModule, NgClass, NgIf],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent { 
+  statusSideBar: boolean = false;
 
+  toggleSideBar() {
+    console.log(this.statusSideBar)
+    this.statusSideBar =!this.statusSideBar;
+  }
 }
