29fd6a13bc3ab0c19c82f0f051c786ea27d3b9be
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-05-08 17:05:34 -0300

    altera o ip

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'http://relatorios.netlinetelecom.com.br:60012',
+    target: 'http://127.0.0.1:60012',
     secure: false,
     logLevel: 'debug',
     pathRewrite: { '^/api': '' }
