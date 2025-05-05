8901f08f3b98180b82ad574d679ddfa194e502b7
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-05-05 09:16:32 -0300

    feat: ajusta visualizacao dos menus conforme permissao

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -29,6 +29,11 @@ export class SidebarComponent implements OnInit, AfterViewInit {
   data_user = this.authService.decodedToken();
   permissions: string[] = this.data_user.permissoes || [];
 
+  permissoesBonificacao: string [] = ['usuarios.dashboard_produtividade_gratificacao', 'usuarios.dashboard_estrutura', 'usuarios.dashboard_instalacao_suporte'];
+
+  permissoesGestao: string [] = ['usuarios.dashboard_auditoria', 'usuarios.dashboard_cgr', 'usuarios.dashboard_comercial', 'usuarios.dashboard_leads', 'usuarios.dashboard_marketing'];
+
+
   ngOnInit(): void {
     initFlowbite();
   }
@@ -36,46 +41,6 @@ export class SidebarComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     initFlowbite();
   }
-
-  menuItems: iMenuItemDrawer[] = [
-    { textMenu: 'Auditoria', routeActive: '/auditoria', permission: 'usuarios.dashboard_auditoria', icon: 'bi bi-bookmark-check-fill' },
-
-    { textMenu: 'Bonificação dos técnicos', routeActive: '/gratificacao_produtividade', permission: 'usuarios.dashboard_produtividade_gratificacao', icon: 'bi bi-currency-dollar' },
-
-    { textMenu: 'Bonificação estrutura', routeActive: '/gratificacao_produtividade_estrutura', permission: 'usuarios.dashboard_estrutura', icon: 'bi bi-currency-dollar' },
-
-    { textMenu: 'Bonificação instalação', routeActive: '/gratificacao_produtividade_instalacao', permission: 'usuarios.dashboard_instalacao_suporte', icon: 'bi bi-currency-dollar' },
-
-    { textMenu: 'Contas a receber', routeActive: '/contas_a_receber', permission: 'usuarios.dashboard_contas_receber', icon: 'bi bi-piggy-bank-fill'},
-
-    { textMenu: 'Estrutura', routeActive: '/estrutura', permission: 'usuarios.dashboard_estrutura', icon: 'bi bi-building'},
-
-    { textMenu: 'Financeiro / Service Desk', routeActive: '/financeiro_service_desk', permission: 'usuarios.dashboard_financeiro_service_desk', icon: 'bi bi-headset'},
-
-    { textMenu: 'Faturamento', routeActive: '/faturamento', permission: 'usuarios.dashboard_faturamento', icon: 'bi bi-boxes'},
-
-    { textMenu: 'Instalação / Suporte', routeActive: '/instalacao_suporte', permission: 'usuarios.dashboard_instalacao_suporte', icon: 'bi bi-gear-fill'},
-
-    { textMenu: 'Vendas', routeActive: '/vendas', permission: 'usuarios.dashboard_comercial', icon: 'bi bi-cart-fill'},
-
-    { textMenu: 'Direção', routeActive: '/direcao', permission: 'usuarios.dashboard_direcao', icon: 'bi bi-kanban-fill'},
-
-    { textMenu: 'Frota', routeActive: '/relatorio_frota', permission: 'usuarios.dashboard_frota', icon: 'bi bi-car-front-fill'},
-
-    { textMenu: 'Informações de Itaporanga', routeActive: '/relatorio_itaporanga', permission: 'usuarios.dashboard_itaporanga', icon: 'bi bi-geo-fill'},
-
-    { textMenu: 'Leads', routeActive: '/relatorio_leads', permission: 'usuarios.dashboard_leads', icon: 'bi bi-person-fill-add'},
-
-    { textMenu: 'Evolução de meta', routeActive: '/evolucao_meta', permission: 'usuarios.dashboard_tv', icon: 'bi bi-tv'},
-
-    { textMenu: 'Ordens de Serviço', routeActive: '/relatorio_ordens_servico', permission: 'usuarios.dashboard_tv', icon: 'bi bi-chat-text-fill'},
-
-    { textMenu: 'Marketing', routeActive: '/marketing', permission: 'usuarios.dashboard_marketing', icon: 'bi bi-bullseye'},
-
-    { textMenu: 'Recursos Humanos', routeActive: '/relatorio_rh', permission: 'usuarios.dashboard_rh', icon: 'bi bi-clock-history'},
-
-    { textMenu: 'CGR', routeActive: '/relatorio_cgr', permission: 'usuarios.dashboard_cgr', icon: 'bi bi-router-fill'},
-  ];
 
   toggleSideBar() {
     if (!this.statusSideBar) {
